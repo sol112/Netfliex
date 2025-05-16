0880400a05b8a8af8321f3f4d3266017962e5a0b
sol112 <108745240+sol112@users.noreply.github.com>
2025-05-16 09:53:17 -0800

    all done

--- Netfliex-Evgdi/src/App.jsx
+++ Netfliex-Evgdi/src/App.jsx
@@ -1,20 +1,30 @@
 
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
-import './App.css'
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import requests from "./Utilities/requests";
+import Row from "./Components/Row/Row";
 
 function App() {
-  
 
   return (
     <>
       <div>
-      <Header />
-      {/* Your main content */}
-      <div style={{ height: '2000px', padding: '20px' }}>
-        {/* Placeholder for main content */}
-        <h1 style={{ color: 'white' }}>Main Content Goes Here</h1>
-      </div>
+    <Header />
+    <Banner />
+			<Row
+				title="NETFLIX ORIGNALS"
+				isLargeRow
+				fetchUrl={requests.fetchNetflixOriginals}
+			/>
+			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       <Footer />
       </div>
      
