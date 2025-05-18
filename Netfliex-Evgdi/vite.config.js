b38d8db40a40ae63ee23eab1f41dfb92a5f9e40b
sol112 <108745240+sol112@users.noreply.github.com>
2025-05-18 05:16:43 -0800

    configuring vite.config.js

--- Netfliex-Evgdi/vite.config.js
+++ Netfliex-Evgdi/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
+  base: "/Netfliex/"
+  
 })
+
+
+server: {
+  host: true
+}
